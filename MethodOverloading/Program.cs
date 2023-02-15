614ccd848dde8c62a2f7f3ac37cb696cee670889
Tom Settle <tom.k.settle@gmail.com>
2023-02-15 10:45:25 -0600

    Add method w/ overloads.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Method Overloading Exercise!");
+            Console.WriteLine($"Integer form of Add(4,5) is {Add(4,5)}");
+            Console.WriteLine($"Decimal form of Add(3.14,4.25) is {Add(3.14m,4.25m)}");
+            Console.WriteLine($"String form of Add(0,1,True) is {Add(0,1,true)}");
+            Console.WriteLine($"String form of Add(4,1,True) is {Add(4, 1, true)}");
+            Console.WriteLine($"String form of Add(3,1,False) is {Add(3, 1, false)}");
+        }
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        static string Add(int x, int y, bool isDollars)
+        {
+            if (isDollars)
+                return $"{x + y} " + (x + y == 1 ? "dollar" : "dollars");
+            else
+                return $"{x + y}";
         }
     }
 }
